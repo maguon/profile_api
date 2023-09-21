0a1f28b4b8b735d0cdfb3606c2b82ce970dc00b3
maguon <info@myxxjs.com>
2023-09-22 00:53:38 +0800

    update test

--- bl/WechatBl.js
+++ bl/WechatBl.js
@@ -24,13 +24,14 @@ const getUserIdByCode = async(req,res,next) =>{
     if(wechatResultObj.openid != null){
         logger.info(" getUserIdByCode " +wechatResultObj.openid)
         const queryRes = await userInfoDAO.queryUserInfoBase({wechatId:wechatResultObj.openid});
+        console.log(queryRes)
         if(queryRes != null && queryRes.length>0){
             const updateRes = await userInfoDAO.updateUserInfo({id:queryRes[0].id,loginAt:new Date().getTime(),loginDateId : moment(new Date()).format('YYYY-MM-DD')})
-            const newToken = jwtUtil.getUserToken(queryRes[0].id,tokenObj.wechatId)
+            const newToken = jwtUtil.getUserToken(queryRes[0].id,wechatResultObj.openid)
             wechatResultObj.authToken = newToken
         }else{
             const insertRes = await userInfoDAO.createUserInfo({wechatId:wechatResultObj.openid,status:1})
-            const newToken = jwtUtil.getUserToken(insertRes.id,tokenObj.wechatId)
+            const newToken = jwtUtil.getUserToken(insertRes.id,wechatResultObj.openid)
             wechatResultObj.authToken = newToken
         }
         resUtil.successRes(res,wechatResultObj,'')
