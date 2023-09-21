4865aa4689b245c55e5289a385d01e21e0fd581e
maguon <info@myxxjs.com>
2023-09-22 00:40:24 +0800

    update test

--- dao/UserInfoDAO.js
+++ dao/UserInfoDAO.js
@@ -52,7 +52,7 @@ class UserInfoDAO {
     }
 
     static async queryUserInfoBase(params) {
-        let query = `select * from user_info`;
+        let query = `select * from user_info where id != null `;
         let filterObj = {};
         if(params.id){
             query += " and id = ${id} ";
